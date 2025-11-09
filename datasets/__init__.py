07d77ff43b955519852998d1d2344009eee2c245
Thanh Hải <optimus1072005@gmail.com>
2025-11-09 20:06:47 +0700

    remove unused datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,21 +1,11 @@
-from .ml_1m import ML1MDataset
 from .ml_100k import ML100KDataset
 from .beauty import BeautyDataset
-from .video import VideoDataset
-from .sports import SportsDataset
-from .steam import SteamDataset
-from .xlong import XLongDataset
 from .yelp2020 import Yelp2020Dataset
 
 
 DATASETS = {
-    ML1MDataset.code(): ML1MDataset,
     ML100KDataset.code(): ML100KDataset,
     BeautyDataset.code(): BeautyDataset,
-    VideoDataset.code(): VideoDataset,
-    SportsDataset.code(): SportsDataset,
-    SteamDataset.code(): SteamDataset,
-    XLongDataset.code(): XLongDataset,
     Yelp2020Dataset.code(): Yelp2020Dataset,
 }
 
